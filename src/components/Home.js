b8a70fe934cec7b29ef8dc6d7a8c07f67fe7f130
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-08-02 05:27:25 +0100

    implement search feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import { fetchCoins } from '../redux/home/homeSlice';
 
 const Home = () => {
+  const [search, setSearch] = useState('');
   const { isLoading, coins } = useSelector((store) => store.home);
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -26,30 +26,44 @@ const Home = () => {
 
   return (
     <>
-      <h1>Digital Coin Explorer</h1>
+      <h1>Crypto Coin Tracker</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="search coins"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
       <div>
-        {coins.map((coin) => {
-          const { id, symbol, percent_change_1h: hour } = coin;
-
-          return (
-            <button type="button" key={id} onClick={() => details(coin)}>
-              <h3>{symbol}</h3>
-              <p>
-                {hour <= 0 ? (
-                  <>
-                    <FaChevronDown color="red" />
-                    {Math.abs(hour)}
-                  </>
-                ) : (
-                  <>
-                    <FaChevronUp color="green" />
-                    {Math.abs(hour)}
-                  </>
-                )}
-              </p>
-            </button>
-          );
-        })}
+        {coins
+          .filter((coin) => {
+            const { symbol } = coin;
+            return search.toLowerCase() === ''
+              ? coin
+              : symbol.toLowerCase().includes(search);
+          })
+          .map((coin) => {
+            const { id, symbol, percent_change_1h: hour } = coin;
+            // console.log(coin);
+            return (
+              <button type="button" key={id} onClick={() => details(coin)}>
+                <h3>{symbol}</h3>
+                <p>
+                  {hour <= 0 ? (
+                    <>
+                      <FaChevronDown color="red" />
+                      {Math.abs(hour)}
+                    </>
+                  ) : (
+                    <>
+                      <FaChevronUp color="green" />
+                      {Math.abs(hour)}
+                    </>
+                  )}
+                </p>
+              </button>
+            );
+          })}
       </div>
     </>
   );
