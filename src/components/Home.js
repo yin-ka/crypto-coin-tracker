3a497e155fa8267849fe21f7dbcce7b75418b266
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2023-08-02 05:11:45 +0100

    fetch data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchCoins } from '../redux/home/homeSlice';
 
-function Home() {
+const Home = () => {
+  const { isLoading, coins } = useSelector((store) => store.home);
+  console.log(isLoading, coins);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isLoading === false) dispatch(fetchCoins());
+  }, [isLoading, dispatch]);
+
+  if (isLoading) {
+    <div>
+      <h1>Loading...</h1>
+    </div>;
+  }
+
   return (
-    <div>Home</div>
+    <>
+      <h1>Digital Coin Explorer</h1>
+      <div>
+        {coins.map((coin) => {
+          const { id, symbol, percent_change_1h: hour } = coin;
+          console.log(coin);
+          return (
+            <button type="button" key={id}>
+              <h3>{symbol}</h3>
+              <p>{hour}</p>
+            </button>
+          );
+        })}
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
--- src/redux/home/homeSlice.js
+++ src/redux/home/homeSlice.js
@@ -1,14 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseUrl = 'https://api.coinlore.net/api/tickers/';
 
 const initialState = {
-  coin: [],
+  coins: [],
   isLoading: false,
 };
 
+export const fetchCoins = createAsyncThunk(
+  'coin/fetchCoins',
+  async (thunkAPI) => {
+    try {
+      const res = await axios.get(baseUrl);
+      const coin = await res.data.data;
+      return coin;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  },
+);
+
 const homeSlice = createSlice({
-  name: 'home',
+  name: 'coin',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCoins.pending, (state) => ({ ...state, loading: true }))
+      .addCase(fetchCoins.fulfilled, (state, { payload }) => ({
+        ...state,
+        coins: payload,
+      }));
+  },
 });
 
 export default homeSlice.reducer;
